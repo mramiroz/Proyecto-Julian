d6268901a74aa23f3a152327676a36d93737e060
david <davidortegahoyas@gmail.com>
2024-01-28 18:00:57 +0100

    He arreglado lo del css.

--- resources/views/layouts/plantillaHeader.blade.php
+++ resources/views/layouts/plantillaHeader.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+@vite(['resources/css/assets/header.css'])
     <link rel="stylesheet" href="{{ asset('css/assets/header.css') }}">
 
     @yield('styles')
