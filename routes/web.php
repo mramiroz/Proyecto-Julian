45146d357366a496dfe8caf3521743c9534be8c3
AlexSava9 <savaa937@gmail.com>
2024-01-29 17:29:14 +0100

    corrigo web

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductoController;
-use App\Htpp\Controllers\UsuarioController;
+use App\Http\Controllers\UsuarioController;
 
 /*
 |--------------------------------------------------------------------------
