913ed6d78be205560d92775edbd1a7e298492ac6
AlexSava9 <savaa937@gmail.com>
2024-01-31 16:40:59 +0100

    CuidadoPersonal plantilla

---
+++ resources/views/productos/cuidadoPersonal.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.plantillaCuidadoPersonal')
+@section('title','Productos')
+@section('header')
+    @include('_components.header')
+@endsection
+@section('categorias')
+    @include('_components.categorias')
+@endsection
+@section('productos-cuidadoPersonal')
+    @include('_components.productos')
+@endsection
+@section('footer')
+    @include('_components.footer')
+@endsection
