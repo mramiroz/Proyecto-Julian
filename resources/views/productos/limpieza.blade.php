6bb90edf28b1d5b3bb184849cdca0955ad7798d7
DavidOrtegaH <davidortegahoyas@gmail.com>
2024-01-31 16:39:43 +0100

    He puesto la categoria de Limpieza

---
+++ resources/views/productos/limpieza.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.plantillaCuerpo')
+@section('title','Productos')
+@section('header')
+    @include('_components.header')
+@endsection
+@section('categorias')
+    @include('_components.categorias')
+@endsection
+@section('footer')
+    @include('_components.footer')
+@endsection
