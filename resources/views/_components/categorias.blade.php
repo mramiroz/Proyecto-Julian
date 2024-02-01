c80e9af97ee4da45a70165216d693f4f7e36b2f6
DavidOrtegaH <davidortegahoyas@gmail.com>
2024-02-01 21:28:15 +0100

    Enlazadas las categorias, falta mejorarlo

--- resources/views/_components/categorias.blade.php
+++ resources/views/_components/categorias.blade.php
@@ -4,10 +4,10 @@
     </header>
     <nav id="categorias-nav">
         <ul id="categorias-lista">
-            <li id="categoria-alimentacion"><a href="route('productos.show', 'alimentacion')" id="perfil">Alimentaci&oacute;n</a></li>
-            <li id="categoria-bebidas"><a href="route('productos.show', 'bebidas')" id="perfil">Bebidas</a></li>
-            <li id="categoria-limpieza"><a href="route('productos.show', 'limpieza')" id="perfil">Limpieza</a></li>
-            <li id="categoria-cuidado-personal"><a href="route('productos.show', 'cuidadoPersonal')" id="perfil">Cuidado personal</a></li>
+            <li id="categoria-alimentacion"><a href="{{route('productos.show', 'alimentacion')}}" id="perfil">Alimentaci&oacute;n</a></li>
+            <li id="categoria-bebidas"><a href="{{route('productos.show', 'bebidas')}}" id="perfil">Bebidas</a></li>
+            <li id="categoria-limpieza"><a href="{{route('productos.show', 'limpieza')}}" id="perfil">Limpieza</a></li>
+            <li id="categoria-cuidado-personal"><a href="{{route('productos.show', 'cuidadoPersonal')}}" id="perfil">Cuidado personal</a></li>
         </ul>
     </nav>
 </section>
