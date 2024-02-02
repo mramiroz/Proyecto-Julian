6fee7adf3c6384c5a3f75e6809364747d241724b
DavidOrtegaH <davidortegahoyas@gmail.com>
2024-02-02 17:21:19 +0100

    La barra de categorias ahora es dinámica en función de las categorias presentes en bd

--- resources/views/_components/categorias.blade.php
+++ resources/views/_components/categorias.blade.php
@@ -4,10 +4,18 @@
     </header>
     <nav id="categorias-nav">
         <ul id="categorias-lista">
-            <li id="categoria-alimentacion"><a href="{{route('productos.show', 'alimentacion')}}" id="perfil">Alimentaci&oacute;n</a></li>
-            <li id="categoria-bebidas"><a href="{{route('productos.show', 'bebidas')}}" id="perfil">Bebidas</a></li>
-            <li id="categoria-limpieza"><a href="{{route('productos.show', 'limpieza')}}" id="perfil">Limpieza</a></li>
-            <li id="categoria-cuidado-personal"><a href="{{route('productos.show', 'cuidadoPersonal')}}" id="perfil">Cuidado personal</a></li>
+            @php
+            $categoriasIntroducidas = [];
+            @endphp
+            @foreach($productos as $producto)
+            @if (!in_array($producto->categoria, $categoriasIntroducidas))
+            <li id="categoria"><a href="{{route('productos.show', $producto->categoria)}}"
+                    id="perfil">{{$producto->categoria}}</a></li>
+            @php
+            array_push($categoriasIntroducidas, $producto->categoria);
+            @endphp
+            @endif
+            @endforeach
         </ul>
     </nav>
 </section>
