83b580c6cc948efe4820aba3a967ef7402c53edc
DavidOrtegaH <davidortegahoyas@gmail.com>
2024-02-02 17:59:08 +0100

    Arreglado el css de los form

--- resources/views/_components/login.blade.php
+++ resources/views/_components/login.blade.php
@@ -1,13 +1,10 @@
-<body>
-    <form action="/usuario/validarUsuario" method="POST">
+<form id="login" action="/usuario/validarUsuario" method="POST">
         @csrf
-        <label for="">Correo: </label>
+        <label for="email">Correo: </label>
         <input type="email" name="email" id="email">
         <br>
-        <label for="">Contraseña: </label>
+        <label for="password">Contraseña: </label>
         <input type="password" name="password" id="password">
         <br>
-        <input type="submit" value="Ingresar">
-    </form>
-</body>
-
+        <input type="submit" id="submit" value="Ingresar">
+</form>
