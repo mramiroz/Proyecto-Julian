83649eea3fc4a84a56335a8692859a44e1d2037b
AlexSava9 <savaa937@gmail.com>
2024-02-02 17:37:57 +0100

    realizo login

--- resources/views/usuario/login.blade.php
+++ resources/views/usuario/login.blade.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <form action="/usuario/validarUsuario" method="POST">
-        @csrf
-        <label for="">Correo: </label>
-        <input type="email" name="email" id="email">
-        <br>
-        <label for="">Contraseña: </label>
-        <input type="password" name="password" id="password">
-        <br>
-        <input type="submit" value="Ingresar">
-    </form>
-</body>
-</html>
+@extends('layouts.plantillaCuerpo')
+@section('title','Login')
+@section('content')
+    @include('_components.login')
+@endsection
