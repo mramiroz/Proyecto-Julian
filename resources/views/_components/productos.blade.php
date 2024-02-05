a232b66c9941e43a061172f1d8a3101ff6df689b
mramiroz <manuelramzab@gmail.com>
2024-02-05 08:19:52 +0100

    modificando estilos

--- resources/views/_components/productos.blade.php
+++ resources/views/_components/productos.blade.php
@@ -2,21 +2,23 @@
     <header id="productos-expuestos-cabecera">
         <h1>Algunos de nuestros productos:</h1>
     </header>
-    @foreach($productos as $producto)
     <article class="productos-expuestos-tarjeta">
-        <section>
+        @foreach($productos as $producto)
+        <section class="producto">
             <figure class="figure-producto">
-                <img src="{{$producto->imagen}}" alt="leche">
+                <img src="{{$producto->imagen}}" alt="{{$producto->nombre}}">
             </figure>
-            <h2>{{$producto->nombre}}</h2>
-            <h6 class="id">id: {{$producto->id}}</h6>
-            <p class="precio">{{$producto->importe}}€/l</p>
-            <form action="{{ route('carrito.add') }}" method="POST">
-                @csrf
-                <input type="hidden" name="product_id" value="{{ $producto->id }}">
-                <button type="submit" value="Submit" name="incluir" class="incluir-carrito">Añadir al carrito</button>
-            </form>
+            <div class="info-producto">
+                <h2>{{$producto->nombre}}</h2>
+                <h6 class="id">id: {{$producto->id}}</h6>
+                <p class="precio">{{$producto->importe}}€/l</p>
+                <form action="{{ route('carrito.add') }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="product_id" value="{{ $producto->id }}">
+                    <button type="submit" value="Submit" name="incluir" class="incluir-carrito">Añadir al carrito</button>
+                </form>
+            </div>
         </section>
+        @endforeach
     </article>
-    @endforeach
 </section>
