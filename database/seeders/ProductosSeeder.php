92b8647bdb196ace598225b39a7759ac8e035c8b
david <davidortegahoyas@gmail.com>
2024-02-11 14:58:35 +0100

    incluidas urls seguras

--- database/seeders/ProductosSeeder.php
+++ database/seeders/ProductosSeeder.php
@@ -17,56 +17,56 @@ class ProductosSeeder extends Seeder
             'nombre' => 'Leche',
             'descripcion' => 'Leche entera',
             'categoria' => 'alimentacion',
-            'imagen' => 'images/Leche.jpg',
+            'imagen' => 'https://ucarecdn.com/1c420ba1-3236-49d7-9eb8-5ce6292f8b75/-/preview/500x500/-/quality/smart/-/format/auto/',
             'importe' => 0.75
         ]);
         Producto::create([
             'nombre' => 'Pan',
             'descripcion' => 'Pan de molde',
             'categoria' => 'alimentacion',
-            'imagen' => 'https://m.media-amazon.com/images/I/716lpUzkOiL.jpg',
+            'imagen' => 'https://ucarecdn.com/66379d3d-d0fd-4eb8-ab4e-bf4ca0d1abe8/-/preview/500x500/-/quality/smart/-/format/auto/',
             'importe' => 0.95
         ]);
         Producto::create([
             'nombre' => 'Coca-Cola',
             'descripcion' => 'Coca-Cola',
             'categoria' => 'bebidas',
-            'imagen' => 'https://www.mosbo.es/cdn/shop/products/coca-cola-lata-33-cl-pack-24-und.jpg?v=1679402039&width=1445',
+            'imagen' => 'https://ucarecdn.com/74f38691-06cc-461c-aef5-df701636e019/-/preview/500x500/-/quality/smart/-/format/auto/',
             'importe' => 1.25
         ]);
         Producto::create([
             'nombre' => 'Cerveza',
             'descripcion' => 'Cerveza',
             'categoria' => 'bebidas',
-            'imagen' => 'https://m.media-amazon.com/images/I/41qRmrBrK4L.jpg',
+            'imagen' => 'https://ucarecdn.com/d693d0a9-ff53-4819-8c8b-077a29c27511/-/preview/500x500/-/quality/smart/-/format/auto/',
             'importe' => 0.75
         ]);
         Producto::create([
             'nombre' => 'Detergente',
             'descripcion' => 'Detergente',
             'categoria' => 'limpieza',
-            'imagen' => 'https://static.carrefour.es/hd_350x_/img_pim_food/769169_00_1.jpg',
+            'imagen' => 'https://ucarecdn.com/269848d9-10cc-4e69-a118-f515f5681a1d/-/preview/500x500/-/quality/smart/-/format/auto/',
             'importe' => 1.15
         ]);
         Producto::create([
             'nombre' => 'Lejía',
             'descripcion' => 'Lejía',
             'categoria' => 'limpieza',
-            'imagen' => 'https://media.gettyimages.com/id/458577301/es/foto/clorox-lej%C3%ADa.jpg?s=612x612&w=0&k=20&c=43bp9DFFvh-Z7I8nZEYzbqk6uUvWOCeyS4UnhfobaEc=',
+            'imagen' => 'https://ucarecdn.com/73e0a2a2-e28d-41ea-afc0-65555b540e20/-/preview/500x500/-/quality/smart/-/format/auto/',
             'importe' => 1.30
         ]);
         Producto::create([
             'nombre' => 'Gel',
             'descripcion' => 'Gel de ducha',
             'categoria' => 'cuidadoPersonal',
-            'imagen' => 'https://www.perfumeriaslaguna.com/images/products-listing/gel-moussel-clasico-viaje-bolso-bano-y-ducha.jpg',
+            'imagen' => 'https://ucarecdn.com/d87ca65a-9a18-4369-988b-71e9060a307c/-/preview/500x500/-/quality/smart/-/format/auto/',
             'importe' => 1.25
         ]);
         Producto::create([
             'nombre' => 'Champú',
             'descripcion' => 'Champú',
             'categoria' => 'cuidadoPersonal',
-            'imagen' => 'https://www.grutinetpro.com/articulos/lg/lg_309554_0.jpg',
+            'imagen' => 'https://ucarecdn.com/0709a5f8-762b-4fc8-bb69-7da21e019f6a/-/preview/500x500/-/quality/smart/-/format/auto/',
             'importe' => 1.25
         ]);
     }
