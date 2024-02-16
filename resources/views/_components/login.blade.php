a6ba33eb48832fed1e31f446f1bdebf490daf132
DavidOrtegaH <davidortegahoyas@gmail.com>
2024-02-16 19:44:47 +0100

    Actualizados login y register

--- resources/views/_components/login.blade.php
+++ resources/views/_components/login.blade.php
@@ -1,3 +1,5 @@
+<h1>Iniciar sesi&oacute;n</h1>
+
 <form id="login" action="/usuario/validarUsuario" method="POST">
         @csrf
         <label for="email">Correo: </label>
@@ -6,6 +8,8 @@
         <label for="password">Contraseña: </label>
         <input type="password" name="password" id="password">
         <br>
+        <fieldset id="botones-form">
         <input type="submit" id="submit" value="Ingresar">
         <input type="button" id="submit" value="Registrarse" onclick="location.href='/register';">
+        </fieldset>
 </form>
--- resources/views/_components/register.blade.php
+++ resources/views/_components/register.blade.php
@@ -1,10 +1,12 @@
+<h1>Crear cuenta</h1>
+
 <form id="register" action="/usuario/create" method="POST">
         @csrf
         <label for="nombre">Nombre: </label>
-        <input type="text" name="nombre" id="nombre">
+        <input type="text" name="nombre" id="name">
         <br>
         <label for="apellidos">Apellido: </label>
-        <input type="text" name="apellidos" id="apellidos">
+        <input type="text" name="apellidos" id="surnames">
         <br>
         <label for="direccion">Dirección: </label>
         <input type="text" name="direccion" id="direccion">
@@ -15,5 +17,7 @@
         <label for="password">Contraseña: </label>
         <input type="password" name="password" id="password">
         <br>
+        <fieldset id="botones-form">
         <input type="submit" id="submit" value="Registrarse">
+        </fieldset>
 </form>
