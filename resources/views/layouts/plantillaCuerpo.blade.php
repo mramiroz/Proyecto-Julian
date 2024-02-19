b73bfece4d93c0d5b815f0c9084897939499310c
DavidOrtegaH <davidortegahoyas@gmail.com>
2024-02-19 19:32:55 +0100

    Arreglado el import del font de emojis

--- resources/views/layouts/plantillaCuerpo.blade.php
+++ resources/views/layouts/plantillaCuerpo.blade.php
@@ -25,10 +25,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Pacifico&family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Noto+Color+Emoji&family=Pacifico&family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap"
         rel="stylesheet">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
+
 <body>
     @yield('header')
     <main>
