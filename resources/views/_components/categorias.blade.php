ebaa93034e09fd87627ef982a24dd5ccdcf8c0d0
mramiroz <manuelramzab@gmail.com>
2024-02-20 07:56:23 +0100

    cambios

--- resources/views/_components/categorias.blade.php
+++ resources/views/_components/categorias.blade.php
@@ -1,12 +1,9 @@
 <section id="categorias">
-    <header id="categorias-cabecera">
-        <h2>Categor&iacute;as</h2>
-    </header>
     <nav id="categorias-nav">
         <ul id="categorias-lista">
             @foreach($categorias as $categoria)
-            <li id="categoria"><a href="{{route('categoria.show', $categoria->route)}}"
-                    id="perfil">{{$categoria->nombre}}</a></li>
+            <a href="{{route('categoria.show', $categoria->route)}}"
+                id="categoria"><li>{{$categoria->nombre}}</li></a>
             @endforeach
         </ul>
     </nav>
--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -2,9 +2,9 @@
 @section('title','Productos')
 @section('header')
     @include('_components.header')
+    @include('_components.categorias', ['categorias' => $categorias])
 @endsection
 @section('content')
-    @include('_components.categorias', ['categorias' => $categorias])
     <h2>Bienvenido {{ session('usuario.nombre')}} !!</h2>
     @include('_components.productos', ['productos' => $productos])
 @endsection
